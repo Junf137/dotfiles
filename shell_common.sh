8bec8cdb2cdfb27bd752d8ec36ddbabbee007bfe
Junfeng Lei <junf137@outlook.com>
2025-07-14 13:37:13 -0400

    [feat] add alias to activate mm_selfsup virtual environment

--- shell_common.sh
+++ shell_common.sh
@@ -179,3 +179,6 @@ alias gsa="git stash apply"
 
 # tmuxp: load session
 alias mux="tmuxp load"
+
+# Activate venv in computer canada cluster
+alias venv_mm_selfsup="source $DOT_FILES/utils/venv_mm_selfsup"
